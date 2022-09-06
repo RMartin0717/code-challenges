9c0f81e0928acb0463a2e277a4b6642e1986529a
Riley Martin <riley.willow@gmail.com>
2022-09-06 13:16:23 -0500

    Clean up code

--- challenges/test/twoSum-test.js
+++ challenges/test/twoSum-test.js
@@ -4,7 +4,6 @@ var TwoSum = require('../exercises/twoSum.js');
 describe('Two Sum', function() {
   it('should return indices of the two numbers such that they add up to target', function() {
     let twoSum = new TwoSum()
-    console.log(twoSum)
     assert.deepEqual(twoSum.twoSum([2,7,11,15], 9), [0,1])
   })
 })
